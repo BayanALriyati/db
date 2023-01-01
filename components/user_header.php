cf9621edad28404bcde043596ab0ce29eab7aa76
unknown <ahmadzytoon@outlook.com>
2023-01-01 14:07:38 +0300

    edit

--- components/user_header.php
+++ components/user_header.php
@@ -42,7 +42,7 @@
 
       <div class="profile">
          <?php          
-            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE user_id = ?");
             $select_profile->execute([$user_id]);
             if($select_profile->rowCount() > 0){
             $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
