5d4c765e6fbfa3444111b4b2102b978934603a79
unknown <salwayehyaabuafash@gmail.com>
2023-01-01 19:38:05 +0200

    fix footer & header

--- components/footer.php
+++ components/footer.php
@@ -3,7 +3,7 @@
    <section class="grid">
 
       <div class="box">
-         <h3>quick links</h3>
+         <h3>Quick links</h3>
          <a href="home.php"> <i class="fas fa-home"></i> home</a>
          <a href="about.php"> <i class="fas fa-question"></i> about</a>
          <a href="shop.php"> <i class="fas fa-shop"></i> shop</a>
@@ -11,26 +11,18 @@
       </div>
      
       <div class="box">
-         <h3>extra links</h3>
+         <h3>Extra links</h3>
          <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
          <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
          <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
          <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
       </div>
 
-      <!-- <div class="box">
-         <h3>contact us</h3>
-         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123 456 7899</a>
-         <a href="tel:11122233333"><i class="fas fa-phone"></i> +111 222 3333</a>
-         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> shaikh@gmail.com</a>
-         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </a>
-      </div> -->
-
       <div class="box">
-         <h3>follow us</h3>
-         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
-         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
-         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <h3>Follow us</h3>
+         <a href="https://web.facebook.com/"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i>instagram</a>
       </div>
 
    </section>
