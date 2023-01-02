914dcf3319b3104c0f49f2dadfe99f64dd52fb55
unknown <salwayehyaabuafash@gmail.com>
2023-01-02 16:23:34 +0200

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,23 +1,38 @@
 <footer class="footer">
 
-   <section class="grid">
+  
+
+            <div class="social">
+               <a class="icon" href="https://web.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+               <a class="icon" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+               <a class="icon" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+            </div>
 
 
-      <div class="box">
-      <a href="home.php" class="logo logo1" >FELUX<span>.</span ></a>
-      </div>
-     
+             <ul class="list">
+               <li>
+                  <a  href="home.php">Home</a>
+               </li>
+               <li>
+                  <a href="shop.php">Shop</a>
+               </li>
+               <li>
+                  <a href="about.php">About</a>
+               </li>
+               <li>
+                  <a href="contact.php">Contact</a>
+               </li>
+             </ul>
+
+                 <p class=copyright>
+               © 2023 FeLux
+                 </p>
    
-      <div class="box">
-         <h3>Follow us</h3>
-         <a href="https://web.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-         <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
-         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-      </div>
+      
 
    </section>
 
-   <div><H3 class="copyright">© 2023 FELUX</H3></div>
+   
  
 
 </footer>
