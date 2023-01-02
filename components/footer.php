86a6b1c48fc799d738bdc7a84b9ec5bfc8085a3f
unknown <salwayehyaabuafash@gmail.com>
2023-01-02 14:39:40 +0200

    test

--- components/footer.php
+++ components/footer.php
@@ -2,30 +2,22 @@
 
    <section class="grid">
 
-      <div class="box">
-         <h3>Quick links</h3>
-         <a href="home.php"> <i class="fas fa-home"></i> home</a>
-         <a href="about.php"> <i class="fas fa-question"></i> about</a>
-         <a href="shop.php"> <i class="fas fa-shop"></i> shop</a>
-         <a href="contact.php"> <i class="fas fa-address-card"></i> contact</a>
-      </div>
-     
-      <div class="box">
-         <h3>Extra links</h3>
-         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
-         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
-         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
-         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
-      </div>
 
       <div class="box">
+      <a href="home.php" class="logo logo1" >FELUX<span>.</span ></a>
+      </div>
+     
+   
+      <div class="box">
          <h3>Follow us</h3>
-         <a href="https://web.facebook.com/"><i class="fab fa-facebook-f"></i>facebook</a>
-         <a href="https://twitter.com/"><i class="fab fa-twitter"></i>twitter</a>
-         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://web.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+         <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
       </div>
 
    </section>
 
+   <div><H3 class="copyright">© 2023 FELUX</H3></div>
+ 
 
 </footer>
